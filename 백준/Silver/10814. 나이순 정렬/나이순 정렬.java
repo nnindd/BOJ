ccc1858ec740db1f341fd0b6650fe671d35a9530
nnindd <62933478+nnindd@users.noreply.github.com>
2023-01-12 22:53:50 +0900

    [Silver V] Title: 나이순 정렬, Time: 388 ms, Memory: 39820 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -4,43 +4,58 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static class Person implements Comparable<Person>{
-		int age;
-		int idx;
-		String name;
 
-		public Person(int age, int idx, String name) {
-			this.age = age;
-			this.idx = idx;
-			this.name = name;
-		}
-
-		@Override
-		public int compareTo(Person o) {
-			if(this.age == o.age) return this.idx - o.idx; 
-			return this.age - o.age;
-		}
-
-		@Override
-		public String toString() {
-			return age + " " + name;
-		}
-	}
+//	static class Person implements Comparable<Person>{
+//		int age;
+//		int idx;
+//		String name;
+//
+//		public Person(int age, int idx, String name) {
+//			this.age = age;
+//			this.idx = idx;
+//			this.name = name;
+//		}
+//
+//		@Override
+//		public int compareTo(Person o) {
+//			if(this.age == o.age) return this.idx - o.idx; 
+//			return this.age - o.age;
+//		}
+//
+//		@Override
+//		public String toString() {
+//			return age + " " + name;
+//		}
+//	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
+		// 배열에 나이 이름 순서로 저장
+		StringBuilder[] builders = new StringBuilder[201];
+
 		StringTokenizer tokens;
-		PriorityQueue<Person> pq = new PriorityQueue<>();
+		int age;
+		String name;
 		for (int i = 0; i < N; i++) {
 			tokens = new StringTokenizer(br.readLine());
-			pq.add(new Person(Integer.parseInt(tokens.nextToken()), i, tokens.nextToken()));
+
+			age = Integer.parseInt(tokens.nextToken());
+			name = tokens.nextToken();
+
+			if (builders[age] == null) {
+				builders[age] = new StringBuilder();
+			}
+
+			builders[age].append(age).append(" ").append(name).append("\n");
 		}
-		
-		while(!pq.isEmpty()) {
-			System.out.println(pq.poll().toString());
+
+		for (StringBuilder sb : builders) {
+			if (sb != null) {
+				sb.deleteCharAt(sb.length() -1);
+				System.out.println(sb);
+			}
 		}
 
 	}
