ba1799005d740b9c45e6ce1c48fcf1c16c901136
nnindd <62933478+nnindd@users.noreply.github.com>
2023-01-17 22:59:17 +0900

    Update Hashing.java

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -8,13 +8,15 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		String str = br.readLine();
 		
-		int r = 31;
 		long M = 1234567891;
 		
 		long result = 0;
+		long mul = 1;
 		
-		for(int i=0; i<str.length(); i++) {
-			result += (str.charAt(i) - 'a' + 1) * Math.pow(r, i) % M;
+		for(int i=0; i<N; i++) {
+			result += (str.charAt(i)-'a'+1) * mul % M;
+			
+			mul = mul * 31 % M;
 		}
 		System.out.println(result % M);
 		
