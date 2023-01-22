c9bfff976cb98291e4c50ccfec7f2dd1f6895490
nnindd <62933478+nnindd@users.noreply.github.com>
2023-01-22 21:18:23 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -15,28 +12,30 @@ public class Main {
         int N = Integer.parseInt(tokens.nextToken());
         int K = Integer.parseInt(tokens.nextToken());
 
-        Queue<Integer> q = new ArrayDeque<>();
+        //(현재 위치에서 +3번째)        k번째 % 총인원 = 자리수
+        //1 2 3 4 5 6 7 >> 3    3   3 % 7 = 3
+        //1 2 4 5 6 7   >> 6    5   3+3-1 % 6 = 5
+        //1 2 4 5 7     >> 2    2   3+5-1 % 5 = 2
+        //1 4 5 7       >> 7    4   3+2-1 % 4 = 0?
+        //1 4 5         >> 5    3   3+0-1 % 3 =
+        //4             >> 1    1
+        //              >> 4    1
+
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 1; i <= N; i++) {
-            q.offer(i);
+            list.add(i);
         }
 
         sb.append("<");
-        int cnt = 0;
-        while (!q.isEmpty()){
-            if(cnt == K-1){
-                sb.append(q.poll()).append(", ");
-                cnt = 0;
-                continue;
-            }
-            q.offer(q.poll());
-            cnt++;
+        int index = 0;
+        while (list.size() > 0) {
+            index = (K + index - 1) % N; //자리수 저장
+            sb.append(list.remove(index)).append(", ");
+            N--;
         }
-
         sb.setLength(sb.length()-2);
         sb.append(">");
 
         System.out.println(sb);
-
     }
-
 }
