cd45a084005e735269ed4637e251ea1eeceb5d4f
nnindd <62933478+nnindd@users.noreply.github.com>
2023-01-27 09:28:19 +0900

    [Silver II] Title: 좌표 압축, Time: 1856 ms, Memory: 358908 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -10,22 +12,24 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
+        int[] sorted = new int[N];
 
         StringTokenizer tokens = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(tokens.nextToken());
+            arr[i] = sorted[i] = Integer.parseInt(tokens.nextToken());
         }
 
-        //집합에 넣어서 자동 정렬
-        TreeSet<Integer> set = new TreeSet<>();
-        for(int i : arr){
-            set.add(i);
-        }
+        //정렬
+        Arrays.sort(sorted);
 
         //맵에 인덱스랑 같이 넣음
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < N; i++) {
-            map.put(set.pollFirst(), i); //정렬된 배열값과 인덱스 순서로 넣음
+        int index = 0;
+        for (int i : sorted) {
+            if(map.containsKey(i)) //이미 있는 경우 제외
+                continue;
+            map.put(i, index); //<배열 값, 순위> 저장
+            index++;
         }
 
         //원본 배열에 해당하는 값 가져옴
@@ -33,7 +37,7 @@ public class Main {
             sb.append(map.get(arr[i])).append(" ");
         }
 
-        sb.setLength(sb.length()-1);
+        sb.setLength(sb.length() - 1);
         System.out.println(sb);
     }
 
