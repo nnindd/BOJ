df3f5c18682a86a6373103ce6eed55be59f9c2aa
nnindd <62933478+nnindd@users.noreply.github.com>
2023-02-27 20:38:39 +0900

    [Silver III] Title: N과 M (8), Time: 128 ms, Memory: 18800 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokens = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(tokens.nextToken());
+        M = Integer.parseInt(tokens.nextToken());
+        arr = new int[N];
+
+        tokens = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(tokens.nextToken());
+        }
+
+        Arrays.sort(arr);
+        comb(0, 0, "");
+        System.out.println(sb);
+
+    }
+
+    private static void comb(int cnt, int start, String res) {
+        if(cnt==M){
+            sb.append(res).append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            comb(cnt+1, i, res+arr[i]+" ");
+        }
+    }
+}
