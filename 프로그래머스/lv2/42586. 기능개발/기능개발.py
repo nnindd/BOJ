904ad76616fd8bc493f82993c19f687fdffc22bb
nnindd <62933478+nnindd@users.noreply.github.com>
2023-04-14 17:40:10 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,15 @@
+def solution(progresses, speeds):
+    answer = []
+    t, cnt = 0, 0
+    while len(progresses) > 0:
+        if progresses[0] + t*speeds[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            t += 1
+    answer.append(cnt)
+    return answer
