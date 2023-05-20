3d8eaf0cc3458cf999ae0c6fa55baf2afe23d7d4
nnindd <62933478+nnindd@users.noreply.github.com>
2023-05-20 15:35:07 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,11 @@
+select
+    TITLE, 
+    b.BOARD_ID, 
+    REPLY_ID, 
+    r.WRITER_ID, 
+    r.CONTENTS, 
+    DATE_FORMAT(r.CREATED_DATE, "%Y-%m-%d") as CREATED_DATE
+from USED_GOODS_BOARD as b join USED_GOODS_REPLY as r
+on b.BOARD_ID = r.BOARD_ID
+where YEAR(b.CREATED_DATE) = 2022 and MONTH(b.CREATED_DATE) = 10
+order by r.CREATED_DATE, TITLE;
