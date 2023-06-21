4c853baa08656a2c1dc9c06a09cfb9635d8c3ea8
nnindd <62933478+nnindd@users.noreply.github.com>
2023-06-21 23:58:19 +0900

    [Silver IV] Title: solved.ac, Time: 340 ms, Memory: 37088 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,40 +9,20 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         int res = 0;
-        //제외할 값
-        int sub = (int) Math.round(N * 0.15);
-
-        //최대 우선순위큐, 최소 우선순위큐 설정
-        PriorityQueue<Integer> max = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
-            }
-        });
-        PriorityQueue<Integer> min = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1 - o2;
-            }
-        });
-
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            int n = Integer.parseInt(br.readLine());
-            max.offer(n);
-            min.offer(n);
-            res += n; //합
+            arr[i] = Integer.parseInt(br.readLine());
+            res += arr[i];
         }
 
-        //sub 개수 만큼 max, min에서 제외
+        Arrays.sort(arr);
+
+        int sub = (int) Math.round(N * 0.15);
         for (int i = 0; i < sub; i++) {
-            int n = max.poll();
-            res -= n;
-            n = min.poll();
-            res -= n;
+            res -= arr[i];
+            res -= arr[N - i - 1];
         }
 
-        //결과 계산
-        double avg = (res * 1.0) / (N - sub * 2);
-        System.out.println(Math.round(avg));
+        System.out.println(Math.round((res * 1.0) / (N - sub * 2)));
     }
 }
