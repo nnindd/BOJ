2d53f73b0eef788b9c6bdca6577ed4c5583525d8
nnindd <62933478+nnindd@users.noreply.github.com>
2023-06-27 16:47:57 +0900

    [Gold IV] Title: 뱀, Time: 80 ms, Memory: 11896 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,107 +1,87 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, map[][], time[];
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokens;
 
-        int N = Integer.parseInt(br.readLine()); //보드의 크기
-        int[][] map = new int[N+1][N+1];
+        N = Integer.parseInt(br.readLine()); //맵 크기
+        map = new int[N][N];
+        time = new int[10001];
 
-        int K = Integer.parseInt(br.readLine()); //사과의 수
+        int K = Integer.parseInt(br.readLine()); //사과의 개수
+        int x, y;
         for (int i = 0; i < K; i++) {
-            //사과의 위치
             tokens = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(tokens.nextToken()) - 1;
-            int y = Integer.parseInt(tokens.nextToken()) - 1;
-            map[x][y] = 9;
+            x = Integer.parseInt(tokens.nextToken()) - 1;
+            y = Integer.parseInt(tokens.nextToken()) - 1;
+            map[x][y] = 1;
         }
 
-        int L = Integer.parseInt(br.readLine()); //뱀의 방향 전환 횟수
-        int[] pos = new int[10_001];
-
-        int LEFT = 1;
-        int RIGHT = 2;
-
-        for (int i = 0; i < L; i++) {
+        K = Integer.parseInt(br.readLine()); //방향 정보
+        char w;
+        for (int i = 0; i < K; i++) {
             tokens = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(tokens.nextToken()); //초
-            char c = tokens.nextToken().charAt(0); //방향 (L->1 왼쪽 90도, D->2 오른쪽 90도)
-            if (c == 'L') pos[x] = LEFT;
-            else if (c == 'D') pos[x] = RIGHT;
-        }//end input
+            x = Integer.parseInt(tokens.nextToken());
+            w = tokens.nextToken().charAt(0);
 
-        //한칸 이동할때마다 1
-        System.out.println(play(pos, map, N, L, LEFT));
+            time[x] = w == 'D' ? 1 : -1; //왼쪽 -1, 오른쪽 1 저장
+        }
+
+        System.out.println(play());
 
     }
 
-    private static int play(int[] pos, int[][] map, int N, int L, int LEFT) {
-        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; //우 하 좌 상
+    private static int play() {
+        int res = 0; //게임 진행 시간
+        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; //오른쪽으로 시계방향
+        int d = 0; //오른쪽
+        ArrayDeque<int[]> pos = new ArrayDeque<>(); //뱀의 모든 위치 저장
+        pos.offer(new int[]{0, 0}); //머리
+        //자기 자신 표시
+        map[0][0] = 999;
 
-        int t = 1; //시간
-        int d = 0; //방향
-
-        //뱀 위치
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-
-        //뱀 시작 위치
-        q.offer(new int[]{0, 0});
-        map[0][0] = 1;
-
+        int nx, ny;
         while (true) {
-            //뱀 머리 가져옴
-            int[] head = q.pollFirst();
+            res++;
 
-            //맵에 머리와 꼬리 표시
-            map[head[0]][head[1]] = 1;
+            int[] cur = pos.peek();
 
-            int nx = head[0] + dir[d][0];
-            int ny = head[1] + dir[d][1];
+            //1. 머리 늘리기
+            nx = cur[0] + dir[d][0];
+            ny = cur[1] + dir[d][1];
 
-            //탈출 조건 (벽이거나 자기자신)
-            if(!isRange(nx, ny, N) || map[nx][ny] == 1) {
-                break;
-            }
+            //2. 벽인지, 자기자신인지 확인
+            if (!isRange(nx, ny) || map[nx][ny] == 999) break;
 
-            //안먹음
-            //지금 머리 다시 넣음
-            q.offerFirst(head);
+            //3. 사과 유무 확인
 
-            int[] tail = q.pollLast();
-            if(map[nx][ny] == 9) {
-                //꼬리 가져옴 맵에 표시하고 다시 넣어줌
-                map[tail[0]][tail[1]] = 1;
-                q.offerLast(tail);
-            }else{
-                //꼬리 제거 맵에서도 제거
+            if (map[nx][ny] == 0) { //사과를 안먹는 경우
+                int[] tail = pos.pollLast();
                 map[tail[0]][tail[1]] = 0;
             }
-
-            //이동한 머리를 가장 처음에 넣어줌, 맵에 표시함
-            q.offerFirst(new int[]{nx, ny});
-            map[nx][ny] = 1;
-
-            //지금 시간이 L보다 작은지 확인, 배열에서 지금 회전해야 하는지 체크
-            if (t < pos.length && pos[t] != 0) {
-                //인덱스 설정
-                if (pos[t] == LEFT) {
-                    d = d - 1 == -1 ? 3 : d - 1;
-                } else {
-                    d = d + 1 == 4 ? 0 : d + 1;
-                }
+            map[nx][ny] = 999;
+            pos.offerFirst(new int[]{nx, ny});
+            
+            //4. 초마다 방향 전환이 있는지 확인 후 방향을 바꿔줌
+            if (time[res] == -1) {
+                d = (d + 3) % 4; //빼기로 진행하면 마이너스값이 나오기 때문에 3을 더해서 모듈러 연산
+            } else if (time[res] == 1) {
+                d = (d + 1) % 4;
             }
-            t++; //시간 증가
         }
-        return t;
+
+        return res;
     }
 
-    private static boolean isRange(int x, int y, int N) {
-        if (x < 0 || x >= N || y < 0 || y >= N) return false;
+    private static boolean isRange(int x, int y) {
+        if (x < 0 || y < 0 || x >= N || y >= N) return false;
         return true;
     }
 }
