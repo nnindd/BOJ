e201306e7a0469e293d0b20b80b5cca7c0eb3404
nnindd <62933478+nnindd@users.noreply.github.com>
2023-07-21 17:05:02 +0900

    [Silver I] Title: IOIOI, Time: 300 ms, Memory: 117376 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        int res = 0;
+        int idx = 0;
+        int cnt = 0;
+
+        StringBuilder sb;
+        //ioi라면 인덱스를 +2, 아니라면 인덱스를 +1
+        while (idx < M - 2) { //가장 끝 3자리까지만 비교하도록
+            sb = new StringBuilder();
+            for (int i = idx; i < idx + 3; i++) {
+                sb.append(str.charAt(i));
+            }
+
+            if (sb.toString().equals("IOI")) {
+                idx += 2;
+                cnt++;
+
+                if (cnt == N) {//N만큼 검사했다면 결과 갱신
+                    res++;
+                    cnt--; //IOIOIOI인 경우 IOIOI|OI, IO|IOIOI를 비교할 때 cnt를 하나 이전으로 돌려줘야 함
+                }
+            } else {
+                idx++;
+                cnt = 0;
+            }
+        }
+        System.out.println(res);
+    }
+}
