d5364892549142ae7befecd47987b530b560daa5
nnindd <62933478+nnindd@users.noreply.github.com>
2023-08-28 17:22:59 +0900

    [Gold III] Title: 메이즈 러너, Time: 76 ms, Memory: 9000 KB

--- 코드트리/Gold/Main_메이즈러너.java
+++ 코드트리/Gold/Main_메이즈러너.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
     static class Pos {
         int x, y;
