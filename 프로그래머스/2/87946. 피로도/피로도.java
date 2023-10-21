c58fadedfd5c5e59e18449a3a296eafddace3e54
nnindd <62933478+nnindd@users.noreply.github.com>
2023-10-21 22:26:17 +0900

    [level 2] Title: 피로도, Time: 0.12 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    int answer;
+    public int solution(int k, int[][] dungeons) {
+        answer = 0;
+        
+        //순열 구하기
+        int len = dungeons.length;
+        perm(0, new int[len][2], new boolean[len], k, dungeons);
+        
+        return answer;
+    }
+    
+    private void perm(int cnt, int[][] s, boolean[] v, int k, int[][] dungeons){
+        //던전 돌 순서를 정함
+        if(cnt == s.length){
+            answer = Math.max(answer, play(s, k)); //정해진 순서대로 플레이
+            return;
+        }
+        
+        for(int i=0; i<s.length; i++){
+            if(v[i]) continue;
+            v[i] = true;
+            s[cnt] = dungeons[i];
+            perm(cnt + 1, s, v, k, dungeons);
+            v[i] = false;
+        }
+    }
+    
+    private int play(int[][] s, int k){
+        //플레이를 모두 할 수 있으면 회수 돌려줌
+        int res = 0;
+        for(int i=0; i<s.length; i++){
+            if(k >= s[i][0]){
+                res++;
+                k -= s[i][1];
+            }
+        }
+        return res;
+    }
+}
